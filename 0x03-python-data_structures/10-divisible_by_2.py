1d5b1b44d8bab9f527835f9b1a9e82489a783668
Naoufal <naoufal.ikkou@gmail.com>
2023-12-05 06:27:32 -0800

    Update Code

---
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,12 @@
+#!/bin/usr/python3
+def divisible_by_2(my_list=[]):
+
+    if my_lsit is None:
+        return None
+    listdivs = []
+    for i in my_list:
+        if (i % 2) == 0:
+            listdivs.append(True)
+        else:
+            listdivs.append(False)
+        return listdivs
